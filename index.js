bd7f084d234097e5d979f66e1e35dee26674eb40
Kale Leach <kleach1212@gmail.com>
2022-02-26 17:14:36 -0800

    commit tree 1082d614680c38aa8f1f7bfc275b9a452bb3add4

--- index.js
+++ index.js
@@ -9,6 +9,73 @@
  for you to use if you need it!
  */
 
+ 
+const createEmployeeRecord = (array) => {
+  const employeeRecord = {
+    firstName:array[0],
+    familyName:array[1],
+    title:array[2],
+    payPerHour:array[3],
+    timeInEvents:[],
+    timeOutEvents:[]
+  }
+  return employeeRecord;
+}
+
+
+const createEmployeeRecords = (array) => {
+  return array.map((e) => createEmployeeRecord(e))
+}
+
+
+const createTimeInEvent = function(dateStamp){
+  const [date, hour] = dateStamp.split(' ');
+
+  const inEvents = {
+    type: 'TimeIn',
+    hour: parseInt(hour),
+    date: date
+  }
+  this.timeInEvents.push(inEvents)
+//debugger
+  return this
+//debugger Used for checking this
+}
+
+const createTimeOutEvent = function(dateStamp){
+  const [date, hour] = dateStamp.split(' ');
+
+  const outEvents = {
+    type: 'TimeOut',
+    hour: parseInt(hour),
+    date: date
+  }
+  this.timeOutEvents.push(outEvents)
+//debugger
+  return this
+//debugger Used for checking this
+}
+
+const hoursWorkedOnDate = function(date){
+  //debugger
+
+  const timeIn = this.timeInEvents.find((e) => e.date === date)
+  const timeOut = this.timeOutEvents.find((e) => e.date === date)
+
+  return (timeOut.hour - timeIn.hour)/100;
+  //debugger
+}
+
+
+const wagesEarnedOnDate = function(date){
+  //debugger
+  const numHour = hoursWorkedOnDate.call(this,date)
+
+  return this.payPerHour * numHour
+
+  //debugger
+}
+
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -21,3 +88,24 @@ const allWagesFor = function () {
     return payable
 }
 
+const findEmployeeByFirstName = function(array, FirstName){
+  for(let employee of array){
+    if (employee.firstName === FirstName){
+      return employee
+    } else {
+      return undefined
+    }
+  }
+}
+
+const calculatePayroll = function(array){
+  return array.reduce((total, rec) => {
+    return total + allWagesFor.call(rec)
+    
+  },0)
+}
+
+
+
+
+ 
